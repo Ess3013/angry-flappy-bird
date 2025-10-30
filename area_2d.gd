4e17be265bcd464487d58ca0ed132996aa442fbe
Ess3013 <eslamessam3013@gmail.com>
2025-10-30 10:48:59 +0300

    Slingshot mechanic

---
+++ area_2d.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var JUMP_VECTOR = 0
+
+var start : Vector2
+var end : Vector2
+var sling_vector : Vector2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("touch"):
+		start = get_global_mouse_position()
+	if Input.is_action_just_released("touch"):
+		end = get_global_mouse_position()
+		sling_vector = end - start
+		apply_impulse(-sling_vector * JUMP_VECTOR)
