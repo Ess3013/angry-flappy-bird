481427645b17a64725ce3d6b30b69eb51866ca33
Ess <72049713+Ess3013@users.noreply.github.com>
2025-11-03 17:22:37 +0200

    fixed wall motion

--- root.gd
+++ root.gd
@@ -7,3 +7,8 @@ extends Node2D
 func _ready() -> void:
 	# This connection is correct, but the function it calls is not
 	bird.slingshot.connect(func(force): node_2d.move_wall(force))
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print_debug("Wall finished")
+	node_2d.reset_position(node_2d.x_position)
--- wall_pair.gd
+++ wall_pair.gd
@@ -2,15 +2,26 @@ class_name WallPair
 
 extends RigidBody2D
 
+@export var x_position = 2500
+@export var starting_x_position = 2200
+
 var y_position : float
 
 func _ready() -> void:
-	y_position = position.y
-
-func move_wall(force: Vector2): # Good practice to type-hint the arg
+	reset_position(starting_x_position)
+	
+func move_wall(force: Vector2):
 	force.y = 0
 	angular_velocity = 0
 	apply_impulse(-force)
 
 func _process(delta: float) -> void:
 	position.y = y_position
+
+func reset_position(x_position):
+	## TODO Perlin Noise Y Position
+	y_position = randf_range(1080/4, 1080*0.75)
+	position.y = y_position
+	global_position.x = x_position
+	if linear_velocity.x < 0:
+		linear_velocity.x *= 0.8
